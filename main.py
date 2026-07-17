1177e203480637805331cb514ae93f9fd9761277
hisenberg1109 <WJ1317AW@outlook.com>
2026-07-17 10:07:12 +0800

    add

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi.responses import FileResponse
 
 app = FastAPI()
 
-@app.get('/image_query/{id}')
+@app.get('/image_query')
 def img_query(id:int ):
     return FileResponse(f'/{id}.png')
