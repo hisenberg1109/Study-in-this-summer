e51c08b1e7111c6b60031a1a8476a9286fbe34a4
hisenberg1109 <WJ1317AW@outlook.com>
2026-07-17 10:31:23 +0800

    git

--- test2.py
+++ test2.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 
-app=FastAPI
+app=FastAPI()
+@app.get('/index',response_class=HTMLResponse)
+def index():
+    html = 
+    return 
+    "<h1>暑期训练营签到系统<h1>"
