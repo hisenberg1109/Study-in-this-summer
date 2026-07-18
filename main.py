0776afebeee60eaffd0abdf3ce5d128ecf91c1d3
hisenberg1109 <WJ1317AW@outlook.com>
2026-07-18 08:42:41 +0800

    githubtest

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+import json
+from fastapi import FastAPI,Form,Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse,FileResponse
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+with open('pokemon.json','w')as f:
+    pokemon_data  = json.load(f)
 
-@app.get('/image_query')
-def img_query(id:int ):
-    return FileResponse(f'/{id}.png')
+app = FastAPI(title='poken_search')
+app.mount('/pokemon',StaticFiles)
+
+templates = Jinja2Templates(
+    directory='templates'
+
+)
+
+
+@app.get('/index.html', response_class=HTMLResponse)#github提交
+def index(request: Request):
+    return templates.TemplateResponse(
+        'index.html', {
+            'request':request
+        }
+    )
+
+
+@app.post('/pokemon', response_class=HTMLResponse)
+def pokemon(
+    id: int = Form(ge=1, le=898)
+):  
+    
+    # html1 = f'<img src=/pokemon/{id}.png></img>'
+    # html2 = f'<p>名字:{pokemon_data[id]["name"]}</p>'
+    # html3 = f'<p>描述:{pokemon_data[id]["desc"]}</p>'
+    # html = html1 + html2 + html3
+    return ''
+@app.get('/main',response_class=FileResponse)
+def main_py():
+    return FileResponse('mian.py')
+
+
